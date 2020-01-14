773f5bf1001561a03830254eca3d620c00669839
Mohammadreza Ghorbani <reza@firstsource.io>
2020-01-14 17:44:13 +0330

    remove animateChartOverlay function

--- packages/components/src/components/collapsible/utils.js
+++ packages/components/src/components/collapsible/utils.js
@@ -6,16 +6,3 @@ export const positionPropType = {
         'bottom',
     ]),
 };
-
-export const animateChartControls = ({ chart_controls_el, is_open }) => {
-    if (!chart_controls_el) return;
-    if (is_open) {
-        chart_controls_el.style.transition = 'all 0.3s linear';
-        chart_controls_el.style.visibility = 'visible';
-        chart_controls_el.style.opacity    = '1';
-    } else {
-        chart_controls_el.style.transition = 'all 0.3s linear';
-        chart_controls_el.style.visibility = 'hidden';
-        chart_controls_el.style.opacity    = '0';
-    }
-};
